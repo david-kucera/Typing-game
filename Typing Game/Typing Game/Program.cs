408a740b7a9507b6c9119b4f05f98ac455d0b1f9
David Kučera <kucera.david2002@gmail.com>
2023-03-29 16:16:04 +0200

    added coding txt

--- Typing Game/Typing Game/Program.cs
+++ Typing Game/Typing Game/Program.cs
@@ -9,7 +9,10 @@
             // Slovak 50 k dictionary
             //Bank bank = new("C:\\Users\\kucer\\OneDrive\\FRI\\2leto\\Jazyk C# a .NET\\_workspace\\semestralna_praca\\typing-game\\Typing Game\\Typing Game\\slovak_50.txt");
             // Slovak full dict.
-            Bank bank = new("C:\\Users\\kucer\\OneDrive\\FRI\\2leto\\Jazyk C# a .NET\\_workspace\\semestralna_praca\\typing-game\\Typing Game\\Typing Game\\slovak.txt");
+            //Bank bank = new("C:\\Users\\kucer\\OneDrive\\FRI\\2leto\\Jazyk C# a .NET\\_workspace\\semestralna_praca\\typing-game\\Typing Game\\Typing Game\\slovak.txt");
+
+            // Programming words
+            Bank bank = new("C:\\Users\\kucer\\OneDrive\\FRI\\2leto\\Jazyk C# a .NET\\_workspace\\semestralna_praca\\typing-game\\Typing Game\\Typing Game\\programmer.txt");
 
             Gameplay hra = new(Difficulty.EASY, bank);
         }
