fec755ee06dcadfba61e2124707a5a1d1eca7540
David Kučera <kucera.david2002@gmail.com>
2023-05-02 10:13:35 +0200

    wpf summary of the game added with starting values

--- Typing Game/WpfApp1/StatWindow.xaml.cs
+++ Typing Game/WpfApp1/StatWindow.xaml.cs
@@ -23,6 +23,24 @@ namespace TypingGame
         {
             InitializeComponent();
             Show();
+            var wpm = 0;
+            WriteIntoTextBlock(wpm);
+        }
+
+        private void WriteIntoTextBlock(int wpm)
+        {
+            string level = null;
+            if (wpm <= 10) level = "Equivalent to one word every 6 seconds. Learn the proper typing technique and practice to improve your speed.";
+            else if (wpm > 0 && wpm <= 10) level = "Equivalent to one word every 3 seconds. Focus on your technique and keep practising.";
+            else if (wpm > 10 && wpm <= 20) level = "Better, but still below average. Keep practising to improve your speed and accuracy.";
+            else if (wpm > 20 && wpm <= 30) level = "At 41 wpm, you are now an average typist. You still have significant room for improvement.";
+            else if (wpm > 30 && wpm <= 40) level = "Congratulations! You're above average!";
+            else if (wpm > 40 && wpm <= 50) level = "This is the speed required for most jobs. You can now be a professional typist.";
+            else if (wpm > 50 && wpm <= 60) level = "You are way above average and would qualify for any typing job, assuming your accuracy is high enough.";
+            else if (wpm > 60 && wpm <= 70) level = "You're a catch! Any employer looking for a typist would love to have you!";
+            else if (wpm > 70 && wpm <= 80) level = "At this speed, you're probably a gamer, coder or genius. You're doing great!";
+            else if (wpm > 80) level = "You're in the top 1% of typists! Congratulations!";
+            TextBlockLevel.Text = level;
         }
     }
 }
