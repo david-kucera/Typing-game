f508bb2c0bd293e93d27ede7bd9b34edb7f33eba
David Kučera <kucera.david2002@gmail.com>
2023-05-23 17:19:39 +0200

    adds

--- Typing Game/TypingGame.App/obj/Debug/net7.0-windows/StatsWindow.g.i.cs
+++ Typing Game/TypingGame.App/obj/Debug/net7.0-windows/StatsWindow.g.i.cs
@@ -85,7 +85,7 @@ namespace TypingGame.App {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/TypingGame.App;V1.0.0.0;component/statswindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/TypingGame.App;component/statswindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\StatsWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
