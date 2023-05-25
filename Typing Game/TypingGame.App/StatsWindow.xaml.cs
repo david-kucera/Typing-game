022b4c8340f5e02d35c539e886ad23399cacb0fa
David Kučera <kucera.david2002@gmail.com>
2023-05-25 07:45:07 +0200

    localzed stats window

--- Typing Game/TypingGame.App/StatsWindow.xaml.cs
+++ Typing Game/TypingGame.App/StatsWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Globalization;
+using System.IO;
 using System.Windows;
 using System.Windows.Media.Imaging;
 using TypingGame.App.UserData;
@@ -11,12 +12,18 @@ namespace TypingGame.App
     /// </summary>
     public partial class StatsWindow : Window
     {
+        private new const string Language = @"lang.txt";
+        private readonly string _languageCode;
+
         /// <summary>
         /// Constructor that sets up all necessary components and shows the window.
         /// </summary>
         public StatsWindow()
         {
             InitializeComponent();
+            _languageCode = File.ReadAllText(Language);
+            Change_Language();
+
             // Sets the window icon
             // https://cdn-icons-png.flaticon.com/512/945/945414.png
             Uri iconUri = new("icon.ico", UriKind.RelativeOrAbsolute);
@@ -27,6 +34,18 @@ namespace TypingGame.App
             TextBoxNumberOfErrors.Text = reader.GetAverageNumberOfErrors().ToString();
             TextBoxAvgWpm.Text = Math.Round(reader.GetAverageNumberOfWpm(), 2).ToString(CultureInfo.CurrentCulture);
             TextBoxAvgCpm.Text = Math.Round(reader.GetAverageNumberOfCpm()).ToString(CultureInfo.CurrentCulture);
+        }
+
+        private void Change_Language()
+        {
+            ResourceDictionary dictionary = new();
+            dictionary.Source = _languageCode switch
+            {
+                "sk" => new Uri("..\\LanguageResources.sk.xaml", UriKind.Relative),
+                "en" => new Uri("..\\LanguageResources.en.xaml", UriKind.Relative),
+                _ => new Uri("..\\LanguageResources.en.xaml", UriKind.Relative)
+            };
+            Resources.MergedDictionaries.Add(dictionary);
         }
     }
 }
